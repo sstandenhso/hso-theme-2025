335346dc2a8801a0123ae915add47eef0128167d
sstandenhso <sean.standen@henryschein.com>
2024-08-02 12:22:20 -0700

    Fixed errors with header.

--- patterns/header-top-bar.php
+++ patterns/header-top-bar.php
@@ -7,18 +7,35 @@
  */
 ?>
 <!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group"><!-- wp:navigation {"ref":8} /--></div>
 <div class="wp-block-group">
-<figure>
-		<a href="#"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/hso-black.png" alt="<?php esc_attr_e( 'Henry Schein Orthodontics', 'twentytwentyfour' ); ?>">HSO</a>
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group">
+	<!-- wp:image {"width":"auto","height":"30px","linkDestination":"custom"} -->
+	 <figure class="wp-block-image">
+		<a href="#">
+			<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/hso-black.png" alt="<?php esc_attr_e( 'Henry Schein Orthodontics', 'twentytwentyfour' ); ?>">
+		</a>
 	</figure>
-	<figure>
-		<a href="#"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/ot-black.png" alt="<?php esc_attr_e( 'Ortho Technology', 'twentytwentyfour' ); ?>">OT</a>
+	<!-- /wp:image -->
+
+	<!-- wp:image {"width":"auto","height":"25px","linkDestination":"custom"} -->
+	 <figure class="wp-block-image" >
+		 <a href="#">
+			<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/ot-black.png" alt="<?php esc_attr_e( 'Ortho Technology', 'twentytwentyfour' ); ?>">
+		</a>
 	</figure>
-	<figure>
-		<a href="#"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/smilers-black.png" alt="<?php esc_attr_e( 'Smilers Expert', 'twentytwentyfour' ); ?>">Smilers</a>
+	<!-- /wp:image -->
+
+	<!-- wp:image {"width":"auto","height":"35px","linkDestination":"custom"} -->
+	 <figure class="wp-block-image">
+		<a href="#">
+			 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/smilers-black.png" alt="<?php esc_attr_e( 'Smilers Expert', 'twentytwentyfour' ); ?>">
+		</a>
 	</figure>
-<div>
-<!-- /wp:group --></div>
+	<!-- /wp:image -->
+</div>
+<!-- /wp:group -->
+
+</div>
 <!-- /wp:group -->
