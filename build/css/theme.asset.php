16c7886c3986667efdfed32361033f3dda4351b2
sstandenhso <sean.standen@henryschein.com>
2024-08-02 16:15:17 -0700

    Made some adjustments.

--- build/css/theme.asset.php
+++ build/css/theme.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '27e8842b8c533a86bb56');
+<?php return array('dependencies' => array(), 'version' => 'cecdaef33318076a297e');
--- patterns/header-top-bar.php
+++ patterns/header-top-bar.php
@@ -10,35 +10,35 @@
 <div class="wp-block-group">
 
 <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group d-flex align-items-center" id="brand-websites-navigation">
-	<!-- wp:image {"width":"auto","height":"30px","linkDestination":"custom"} -->
-	 <figure class="wp-block-image is-resized border-right px-2 mb-0">
+<div class="wp-block-group d-flex align-items-center flex-wrap flex-sm-nowrap" id="brand-websites-navigation">
+	<!-- wp:image {"linkDestination":"custom"} -->
+	 <figure class="wp-block-image border-right px-1 px-sm-2 mb-0">
 		<a href="#">
-			<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/hso-black.png" alt="<?php esc_attr_e( 'Henry Schein Orthodontics', 'twentytwentyfour' ); ?>" style="width:auto;height:30px;">
+			<img id="hso-logo-link" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/hso-black.png" alt="<?php esc_attr_e( 'Henry Schein Orthodontics', 'twentytwentyfour' ); ?>">
 		</a>
 	</figure>
 	<!-- /wp:image -->
 
 	<!-- wp:paragraph -->
-	<p class="px-2 mb-0">|</p>
+	<p class="px-1 px-sm-2 mb-0">|</p>
 	<!-- /wp:paragraph -->
 
-	<!-- wp:image {"width":"auto","height":"25px","linkDestination":"custom"} -->
-	 <figure class="wp-block-image is-resized border-right px-2 mb-0" >
+	<!-- wp:image {"linkDestination":"custom"} -->
+	 <figure class="wp-block-image border-right px-1 px-sm-2 mb-0" >
 		 <a class="h-100" href="#">
-			<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/ot-black.png" alt="<?php esc_attr_e( 'Ortho Technology', 'twentytwentyfour' ); ?>" style="width:auto;height:25px;">
+			<img id="ot-logo-link" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/ot-black.png" alt="<?php esc_attr_e( 'Ortho Technology', 'twentytwentyfour' ); ?>">
 		</a>
 	</figure>
 	<!-- /wp:image -->
 
 	<!-- wp:paragraph -->
-	<p class="px-2 mb-0">|</p>
+	<p class="px-1 px-sm-2 mb-0">|</p>
 	<!-- /wp:paragraph -->
 
-	<!-- wp:image {"width":"auto","height":"35px","linkDestination":"custom"} -->
-	 <figure class="wp-block-image is-resized px-2 mb-0">
+	<!-- wp:image {"linkDestination":"custom"} -->
+	 <figure class="wp-block-image px-1 px-sm-2 mb-0">
 		<a href="#">
-			 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/smilers-black.png" alt="<?php esc_attr_e( 'Smilers Expert', 'twentytwentyfour' ); ?>" style="width:auto;height:35px;">
+			 <img id="smilers-logo-link" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/smilers-black.png" alt="<?php esc_attr_e( 'Smilers Expert', 'twentytwentyfour' ); ?>">
 		</a>
 	</figure>
 	<!-- /wp:image -->
