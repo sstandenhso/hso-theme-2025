aa65425eed2e39fd508bfca6937890e6fd875a79
sstandenhso <sean.standen@henryschein.com>
2024-08-29 15:19:11 -0700

    Features with icons centered.

---
+++ patterns/features-cols-icons.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Title: HSO Features Columns With Icons
+ * Slug: hso/features-cols-icons
+ * Categories: about
+ * Viewport width: 1400
+ */
+?>
+
+<!-- wp:group {"className":"container","style":{"spacing":{"padding":{"top":"40px","bottom":"40px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group container" style="padding-top:40px;padding-bottom:40px"><!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"26px","fontStyle":"normal","fontWeight":"500"},"spacing":{"padding":{"bottom":"20px"}}}} -->
+<h2 class="wp-block-heading has-text-align-center" style="padding-bottom:20px;font-size:26px;font-style:normal;font-weight:500">Does Your DSO Need Better Ortho Support?</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"15px","fontStyle":"normal","fontWeight":"400"},"spacing":{"padding":{"bottom":"20px"}},"elements":{"link":{"color":{"text":"var:preset|color|hso-grey-80"}}}},"textColor":"hso-grey-80"} -->
+<p class="has-text-align-center has-hso-grey-80-color has-text-color has-link-color" style="padding-bottom:20px;font-size:15px;font-style:normal;font-weight:400">Great news! Our Special Markets team is the best of the best when it comes to helping our DSO partners start or grow their Orthodontics Program. Click Get Started to engage our team for a partnership consultation.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:columns {"className":"container"} -->
+<div class="wp-block-columns container"><!-- wp:column {"layout":{"type":"default"}} -->
+<div class="wp-block-column"><!-- wp:image {"id":265,"width":"30px","sizeSlug":"full","linkDestination":"none","align":"center","className":"icon-center"} -->
+<figure class="wp-block-image aligncenter size-full is-resized icon-center"><img src="http://localhost:8888/wp-content/uploads/2024/08/2-year-price.png" alt="" class="wp-image-265" style="width:30px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"500"}}} -->
+<h3 class="wp-block-heading has-text-align-center" style="font-size:18px;font-style:normal;font-weight:500">Continuing Education Courses (CE)</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"15px","fontStyle":"normal","fontWeight":"400"},"elements":{"link":{"color":{"text":"var:preset|color|hso-grey-80"}}}},"textColor":"hso-grey-80"} -->
+<p class="has-text-align-center has-hso-grey-80-color has-text-color has-link-color" style="font-size:15px;font-style:normal;font-weight:400">We offer a myriad of clinical CE and product education opportunities that feature top KOL speakers including Dr. Luis Carrière, Dr. Mario Chorak &amp; Dr. Bruce McFarlane. We offer on-demand virtual learning, customized in-person education, and/or VIP access to industry leading events such as the HSO Symposium.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"layout":{"type":"default"}} -->
+<div class="wp-block-column"><!-- wp:image {"id":265,"width":"30px","sizeSlug":"full","linkDestination":"none","align":"center","className":"icon-center"} -->
+<figure class="wp-block-image aligncenter size-full is-resized icon-center"><img src="http://localhost:8888/wp-content/uploads/2024/08/2-year-price.png" alt="" class="wp-image-265" style="width:30px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"500"}}} -->
+<h3 class="wp-block-heading has-text-align-center" style="font-size:18px;font-style:normal;font-weight:500">Continuing Education Courses (CE)</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"15px","fontStyle":"normal","fontWeight":"400"},"elements":{"link":{"color":{"text":"var:preset|color|hso-grey-80"}}}},"textColor":"hso-grey-80"} -->
+<p class="has-text-align-center has-hso-grey-80-color has-text-color has-link-color" style="font-size:15px;font-style:normal;font-weight:400">We offer a myriad of clinical CE and product education opportunities that feature top KOL speakers including Dr. Luis Carrière, Dr. Mario Chorak &amp; Dr. Bruce McFarlane. We offer on-demand virtual learning, customized in-person education, and/or VIP access to industry leading events such as the HSO Symposium.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"layout":{"type":"default"}} -->
+<div class="wp-block-column"><!-- wp:image {"id":265,"width":"30px","sizeSlug":"full","linkDestination":"none","align":"center","className":"icon-center"} -->
+<figure class="wp-block-image aligncenter size-full is-resized icon-center"><img src="http://localhost:8888/wp-content/uploads/2024/08/2-year-price.png" alt="" class="wp-image-265" style="width:30px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"500"}}} -->
+<h3 class="wp-block-heading has-text-align-center" style="font-size:18px;font-style:normal;font-weight:500">Continuing Education Courses (CE)</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"15px","fontStyle":"normal","fontWeight":"400"},"elements":{"link":{"color":{"text":"var:preset|color|hso-grey-80"}}}},"textColor":"hso-grey-80"} -->
+<p class="has-text-align-center has-hso-grey-80-color has-text-color has-link-color" style="font-size:15px;font-style:normal;font-weight:400">We offer a myriad of clinical CE and product education opportunities that feature top KOL speakers including Dr. Luis Carrière, Dr. Mario Chorak &amp; Dr. Bruce McFarlane. We offer on-demand virtual learning, customized in-person education, and/or VIP access to industry leading events such as the HSO Symposium.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
