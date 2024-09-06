a592afe2e20b8b8b827bfb769c96e03a2a5d8782
sstandenhso <sean.standen@henryschein.com>
2024-09-06 15:48:25 -0700

    Product Detail Image Carousel and Text section done.  Will implement scrollable on mobile once I have a slider element created.

---
+++ build/js/mobile-nav.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '5dee57d15902a30d156f');
---
+++ patterns/product-details-with-image-carousel.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Title: Product Details With Image Carousel
+ * Slug: hso/product-details-with-image-carousel
+ * Categories: about
+ * Viewport width: 1400
+ */
+?>
+
+<!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:group {"className":"container","style":{"spacing":{"padding":{"top":"40px","bottom":"20px"},"blockGap":"30px"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group container" style="padding-top:40px;padding-bottom:20px"><!-- wp:image {"id":401,"width":"600px","sizeSlug":"large","linkDestination":"none","className":"selected-image"} -->
+<figure class="wp-block-image size-large is-resized selected-image" id="selected-image"><img src="http://localhost:8888/wp-content/uploads/2024/08/01_Motion_Pro_Clear_ANGLE-1024x576.jpg" alt="" class="wp-image-401" style="width:600px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading -->
+<h2 class="wp-block-heading">Carriere Motion Pro</h2>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"level":3} -->
+<h3 class="wp-block-heading">Clear Bite Corrector</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"product-detail-border-left","style":{"typography":{"fontSize":"15px","fontStyle":"normal","fontWeight":"500","lineHeight":"2"},"spacing":{"padding":{"top":"10px","bottom":"10px","left":"30px"},"margin":{"top":"15px","bottom":"15px"}}}} -->
+<p class="product-detail-border-left" style="margin-top:15px;margin-bottom:15px;padding-top:10px;padding-bottom:10px;padding-left:30px;font-size:15px;font-style:normal;font-weight:500;line-height:2">Our mission at Henry Schein Orthodontics is to enable beautiful, healthy smiles through passionate customer collaboration.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"15px"},"spacing":{"padding":{"bottom":"20px"}}}} -->
+<p style="padding-bottom:20px;font-size:15px">Select the button border color to change the other border colors of the section.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"textColor":"contrast","className":"is-style-fill","style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"borderColor":"smilers-green"} -->
+<div class="wp-block-button is-style-fill" id="product-detail-shop-button"><a class="wp-block-button__link has-contrast-color has-text-color has-link-color has-border-color has-smilers-green-border-color wp-element-button">Shop Now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"container","style":{"spacing":{"padding":{"bottom":"40px"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group container" style="padding-bottom:40px"><!-- wp:gallery {"columns":5,"linkTo":"none","sizeSlug":"medium","className":"slideshow-images"} -->
+<figure class="wp-block-gallery has-nested-images columns-5 is-cropped slideshow-images"><!-- wp:image {"id":423,"sizeSlug":"medium","linkDestination":"none","className":"slideshow-image selected"} -->
+<figure class="wp-block-image size-medium slideshow-image selected"><img src="http://localhost:8888/wp-content/uploads/2024/08/01_Motion_Pro_Clear_ANGLE-1-300x169.jpg" alt="" class="wp-image-423"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":425,"sizeSlug":"medium","linkDestination":"none","className":"slideshow-image"} -->
+<figure class="wp-block-image size-medium slideshow-image"><img src="http://localhost:8888/wp-content/uploads/2024/08/02_Motion_Pro_Clear_SIDE-300x169.jpg" alt="" class="wp-image-425"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":426,"sizeSlug":"medium","linkDestination":"none","className":"slideshow-image"} -->
+<figure class="wp-block-image size-medium slideshow-image"><img src="http://localhost:8888/wp-content/uploads/2024/08/03_Motion_Pro_Clear_TOP-300x169.jpg" alt="" class="wp-image-426"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":424,"sizeSlug":"medium","linkDestination":"none","className":"slideshow-image"} -->
+<figure class="wp-block-image size-medium slideshow-image"><img src="http://localhost:8888/wp-content/uploads/2024/08/04_Motion_Pro_Clear_BOTTOM-300x169.jpg" alt="" class="wp-image-424"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":422,"sizeSlug":"medium","linkDestination":"none","className":"slideshow-image"} -->
+<figure class="wp-block-image size-medium slideshow-image"><img src="http://localhost:8888/wp-content/uploads/2024/08/05_Motion_Pro_Clear_Cuspid_Detail_Side-300x169.jpg" alt="" class="wp-image-422"/></figure>
+<!-- /wp:image --></figure>
+<!-- /wp:gallery --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
