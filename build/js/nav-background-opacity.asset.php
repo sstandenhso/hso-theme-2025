ea0727cd1045d7182852ab9f17356d8c9b6537ce
sstandenhso <sean.standen@henryschein.com>
2024-09-09 12:52:28 -0700

    Navigation scroll animation

---
+++ build/js/nav-background-opacity.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'ebfb232b6168bc014f3a');
---
+++ src/js/nav-background-opacity.js
@@ -0,0 +1,15 @@
+const header = document.getElementById('site-header-main');
+const hero = document.querySelector(".banner-hero");
+
+document.addEventListener("DOMContentLoaded", function(arg) {
+    if(hero) {
+        header.classList.add("background-opacity-0");
+    }
+});
+
+document.addEventListener("scroll", e => {
+    if(window.scrollY > 200) {
+        header.style.transition = "all 0.3s ease-in-out"
+        header.classList.remove("background-opacity-0");
+    }
+})
